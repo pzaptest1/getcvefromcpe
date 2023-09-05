2d2502a2de830198c8bb17315b9eafb8e6984cdf
Pali Surdhar Local <pali.surdhar@entrust.com>
2023-09-05 14:26:16 +0100

    accepts a list of products rather than individual

--- getcvefromcpe.py
+++ getcvefromcpe.py
@@ -34,19 +34,17 @@ def generate_cpe_string(package_name, vendor, package_version):
     cpe_string = f"cpe:{cpe_version}:{part}:{vendor}:{product}:{version}:{update}:{edition}:{language}:{sw_edition}:{target_sw}:{target_hw}:{other}"
     return cpe_string
 
-def read_packages_from_excel(filename, sheet_name):
+def read_packages_from_excel(filename, sheet):
     package_data = []
+
+    package_column = None
+    vendor_column = None
+    version_column = None
     
     try:
         workbook = openpyxl.load_workbook(filename)
-        sheet = workbook[sheet_name]
-
-      #  sheet = workbook.active
-        
-        package_column = None
-        vendor_column = None
-        version_column = None
-        
+      
+        sheet = workbook[sheet]
         for row in sheet.iter_rows(min_row=1, max_row=1, values_only=True):
             for index, cell_value in enumerate(row):
                 if cell_value == 'Package':
@@ -55,34 +53,40 @@ def read_packages_from_excel(filename, sheet_name):
                     vendor_column = index + 1
                 elif cell_value == 'Version':
                     version_column = index + 1
-                    
+                        
         if package_column is None or vendor_column is None or version_column is None:
             raise ValueError("Column headers 'Package', 'Vendor', and 'Version' not found.")
-        
+            
         for row in sheet.iter_rows(min_row=2, values_only=True):
             package_name = row[package_column - 1]
             package_name = validate_input(package_name)
             vendor = row[vendor_column - 1]
+            if not vendor:
+                vendor = package_name
             vendor = validate_input(vendor)
             package_version = row[version_column - 1]
             cpe_string = generate_cpe_string(package_name, vendor, package_version)
             package_data.append(cpe_string)
-            
+                    
     except Exception as e:
         print(f"An error occurred: {e}")
-    
+        
     return package_data
 
 if __name__ == "__main__":
     excel_filename = "Book1.xlsx"
-    sheet_name = "nshield5"
+    sheet_name = ['test','product1','product2']
 
-    cpe_strings = read_packages_from_excel(excel_filename, sheet_name)
-    i = 0
-    for cpe_string in cpe_strings:
-        print(f"CPE String: {cpe_string}")
-        r = nvdlib.searchCVE(cpeName = cpe_string)
-        for eachCVE in r:
-           print(eachCVE.id, eachCVE.score, eachCVE.url)
+  
+    for sheet in sheet_name:
+        print(sheet)
+
+        cpe_strings = read_packages_from_excel(excel_filename, sheet)
+    
+        for cpe_string in cpe_strings:
+            print(f"CPE String: {cpe_string}")
+            r = nvdlib.searchCVE(cpeName = cpe_string)
+            for eachCVE in r:
+                print(eachCVE.id, eachCVE.score, eachCVE.url)
 
  
